e03dc8fc34631ecb30bd999af17e35dcadea1437
Michael Karg <michael.karg@iohk.io>
2024-05-08 14:08:58 +0200

    nix svc: drop LDMB specific buggy assertion

--- nix/nixos/cardano-node-service.nix
+++ nix/nixos/cardano-node-service.nix
@@ -715,7 +715,6 @@ in {
   config = mkIf cfg.enable ( let
     stateDirBase = "/var/lib/";
     runDirBase = "/run/";
-    lmdbPaths = filter (x: x != null) (map (e: lmdbDatabasePath e) cfg.instances);
     genInstanceConf = f: listToAttrs (if cfg.instances > 1
       then genList (i: let n = "cardano-node-${toString i}"; in nameValuePair n (f n i)) cfg.instances
       else [ (nameValuePair "cardano-node" (f "cardano-node" 0)) ]); in lib.mkMerge [
@@ -816,10 +815,6 @@ in {
         {
           assertion = !(cfg.systemdSocketActivation && cfg.useNewTopology);
           message = "Systemd socket activation cannot be used with p2p topology due to a systemd socket re-use issue.";
-        }
-        {
-          assertion = (length lmdPaths) == (length (lib.lists.unique lmdbPaths));
-          message   = "When configuring multiple LMDB enabled nodes on one instance, lmdbDatabasePath must be unique.";
         }
       ];
     }
