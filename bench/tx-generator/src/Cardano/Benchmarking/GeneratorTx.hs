ccf071db4a95b0d12f60a7bb95c18b3a18546715
Nadia Yvette Chambers <nadia.chambers@iohk.io>
2024-05-15 07:00:09 +0000

    txgen-mvar: use barrier to avoid bootstrap race
    In principle, the parent could outrun its children and catch them before
    they're finished getting set up wrt. the AsyncBenchmarkControl. So this
    gets them as far as having their array entries initialized before
    proceeding. The logs are way too short, though nothing shows on stderr.

--- bench/tx-generator/src/Cardano/Benchmarking/GeneratorTx.hs
+++ bench/tx-generator/src/Cardano/Benchmarking/GeneratorTx.hs
@@ -36,23 +36,26 @@ import           Cardano.TxGenerator.Types (NumberOfTxs, TPSRate, TxGenError (..
 
 import           Prelude (String)
 
-import qualified Control.Concurrent.STM as STM
-import qualified Control.Monad.STM as STM ()
+import qualified Control.Concurrent.STM.TArray as STM (TArray)
+import qualified Control.Concurrent.STM.TMVar as STM (newEmptyTMVar, newTMVar, putTMVar, takeTMVar)
+import qualified Control.Concurrent.STM.TSem as STM (TSem, newTSem, signalTSemN, waitTSem)
+import qualified Control.Concurrent.STM.TVar as STM ()
+import qualified Control.Monad.STM as STM (atomically)
 import qualified Data.Array.MArray as MArray
 import qualified Data.List as List (intercalate)
 import qualified Data.List.NonEmpty as NE
 import           Data.Text (pack)
 import qualified Data.Time.Clock as Clock
 import           Data.Tuple.Extra (secondM)
-import           GHC.Conc as Conc (labelThread, unsafeIOToSTM)
+import           GHC.Conc as Conc (labelThread)
 
+-- For some reason, stylish-haskell wants to delete this.
 #if MIN_VERSION_base(4,18,0)
--- fromMaybe is imported via Cardano.Prelude
--- However, this configuration actually uses it.
--- import           Data.Maybe (fromMaybe)
+--- fromMaybe is imported via Cardano.Prelude
+--- However, this configuration actually uses it.
+--- import           Data.Maybe (fromMaybe)
 import           GHC.Conc.Sync as Conc (threadLabel)
 #endif
-
 import           Network.Socket (AddrInfo (..), AddrInfoFlag (..), Family (..), SocketType (Stream),
                    addrFamily, addrFlags, addrSocketType, defaultHints, getAddrInfo)
 
@@ -158,29 +161,50 @@ walletBenchmark
 
   txStreamRef <- newMVar $ StreamActive txSource
 
-  (allAsyncs, reportRefs) <- atomically do
+  (allAsyncs, reportRefs, asyncArray, top) <- atomically do
     reportRefs' :: [ReportRef] <- replicateM (fromIntegral numTargets) STM.newEmptyTMVar
     asyncArray' :: STM.TArray Int (ReportRef, (String, AddrInfo))
         <- MArray.newListArray (1, maxBound) . zip reportRefs' $ NE.toList remoteAddresses
-    (_, top)    <- MArray.getBounds asyncArray'
-    allAsyncs' :: STM.TArray Int (Async ()) <- MArray.newArray_ (1, top)
-    forM_ [1 .. top] \k -> do
-      (reportRef, remoteInfo@(remoteName, remoteAddrInfo)) <- MArray.readArray asyncArray' k
-      let errorHandler = handleTxSubmissionClientError traceSubmit remoteInfo reportRef errorPolicy
-          client = txSubmissionClient
+    (_, top')    <- MArray.getBounds asyncArray'
+    allAsyncs' :: STM.TArray Int (Async ()) <- MArray.newArray_ (1, top')
+    pure (allAsyncs', reportRefs', asyncArray', top')
+
+  semaphore :: STM.TSem <- STM.atomically do STM.newTSem 0
+  barrier <- STM.atomically do STM.newTMVar (0 :: Int)
+  let waitBarrier = STM.atomically do
+                      counter' <- STM.takeTMVar barrier
+                      let counter = counter' + 1
+                      STM.putTMVar barrier counter
+                      -- Two for each node, plus the throttler and this thread.
+                      when (counter == 2 * top + 2) do
+                        STM.signalTSemN (fromIntegral $ top + 2) semaphore
+                      STM.waitTSem semaphore
+  forM_ [1 .. top] \k -> do
+    (reportRef, remoteInfo@(remoteName, remoteAddrInfo))
+            <- STM.atomically $ MArray.readArray asyncArray k
+    let errorHandler = handleTxSubmissionClientError traceSubmit remoteInfo reportRef errorPolicy
+        client = txSubmissionClient
                      traceN2N
                      traceSubmit
                      (txStreamSource txStreamRef tpsThrottle)
                      (submitSubmissionThreadStats reportRef)
-          remoteAddrString = show $ addrAddress remoteAddrInfo
-      asyncThread <- Conc.unsafeIOToSTM . async . handle errorHandler $ connectClient remoteAddrInfo client
-      let tid = asyncThreadId asyncThread
-      Conc.unsafeIOToSTM . Conc.labelThread tid $ "txSubmissionClient " ++ show tid ++
+        remoteAddrString = show $ addrAddress remoteAddrInfo
+    asyncThread <- async do waitBarrier
+                            handle errorHandler $ connectClient remoteAddrInfo client
+    let tid = asyncThreadId asyncThread
+    Conc.labelThread tid $ "txSubmissionClient " ++ show tid ++
                             " servicing " ++ remoteName ++ " (" ++ remoteAddrString ++ ")"
-      MArray.writeArray allAsyncs' k asyncThread
-    pure (allAsyncs', reportRefs')
+    STM.atomically do MArray.writeArray allAsyncs k asyncThread
+                      counter <- STM.takeTMVar barrier
+                      STM.putTMVar barrier $ counter + 1
+                      -- Two for each node, plus the throttler and this thread.
+                      when (counter == 2 * top + 2) do
+                        STM.signalTSemN (fromIntegral $ top + 2) semaphore
+                      -- It only needs to signal getting past here, not to wait.
+    pure (allAsyncs, reportRefs)
 
   tpsThrottleThread <- async $ do
+    waitBarrier
     startSending tpsThrottle
     traceWith traceSubmit $ TraceBenchTxSubDebug "tpsLimitedFeeder : transmitting done"
     STM.atomically $ sendStop tpsThrottle
@@ -192,6 +216,7 @@ walletBenchmark
         cancel tpsThrottleThread
         liftIO $ STM.atomically $ sendStop tpsThrottle
 
+  waitBarrier
   return (tpsThrottleThread, allAsyncs, mkSubmissionSummary threadName startTime reportRefs, tpsFeederShutdown)
  where
   traceDebug :: String -> IO ()
